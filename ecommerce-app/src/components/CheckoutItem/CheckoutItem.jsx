098bb17ce5c72d03c51d108e31edb05b4e224b23
jamesbrignull <jamesbrignull@gmail.com>
2020-11-26 08:42:41 +0000

    checkout functionality complete

--- ecommerce-app/src/components/CheckoutItem/CheckoutItem.jsx
+++ ecommerce-app/src/components/CheckoutItem/CheckoutItem.jsx
@@ -1,24 +1,40 @@
 import React from "react";
 import * as S from "./CheckoutItem.style";
 
-function CheckoutItem({ cartItem: { name, imageUrl, price, quantity } }) {
+import { connect } from "react-redux";
+
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem,
+} from "../../redux/Cart/cartActions";
+
+function CheckoutItem({ cartItem, clearItem, addItem, removeItem }) {
+  const { name, imageUrl, price, quantity } = cartItem;
+
   return (
     <S.CheckoutItemContainer>
       <S.ImageContainer>
         <img src={imageUrl} alt="item" />
       </S.ImageContainer>
       <S.TextContainer>{name}</S.TextContainer>
-      {/* <S.QuantityContainer>
+      <S.QuantityContainer>
         <div onClick={() => removeItem(cartItem)}>&#10094;</div>
         <span>{quantity}</span>
         <div onClick={() => addItem(cartItem)}>&#10095;</div>
-      </S.QuantityContainer> */}
+      </S.QuantityContainer>
       <S.TextContainer>{price}</S.TextContainer>
-      {/* <S.RemoveButtonContainer onClick={() => clearItem(cartItem)}>
+      <S.RemoveButtonContainer onClick={() => clearItem(cartItem)}>
         &#10005;
-      </S.RemoveButtonContainer> */}
+      </S.RemoveButtonContainer>
     </S.CheckoutItemContainer>
   );
 }
 
-export default CheckoutItem;
+const mapDispatchToProps = (dispatch) => ({
+  clearItem: (item) => dispatch(clearItemFromCart(item)),
+  addItem: (item) => dispatch(addItem(item)),
+  removeItem: (item) => dispatch(removeItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
